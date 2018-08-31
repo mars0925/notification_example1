1c1e4449d1a44ce5095280aed934f046d42c5dd0
Mars <mars0925@24drs.com>
2018-08-31 14:33:34 +0800

    notificationsmaple4 /**再通知上面顯示大型圖片以及播放媒體  * 播放媒體需要 implementation 'com.android.support:support-media-compat:27.1.1'*/

--- notificationsample4/src/main/java/shd/com/notificationsample4/MainActivity.java
+++ notificationsample4/src/main/java/shd/com/notificationsample4/MainActivity.java
@@ -1,23 +1,29 @@
-package shd.com.notificationsample3;
+package shd.com.notificationsample4;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static shd.com.notificationsample3.App.CHANNEL_1_ID;
-import static shd.com.notificationsample3.App.CHANNEL_2_ID;
-/**顯示更長的文字,背景圖片*/
+import static shd.com.notificationsample4.App.CHANNEL_1_ID;
+import static shd.com.notificationsample4.App.CHANNEL_2_ID;
+
+/**再通知上面顯示大型圖片以及播放媒體
+ * 播放媒體需要 implementation 'com.android.support:support-media-compat:27.1.1'*/
 public class MainActivity extends AppCompatActivity {
     private NotificationManagerCompat notificationManager;
     private EditText editTextTitle;
     private EditText editTextMessage;
+
+    private MediaSessionCompat mediaSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +34,31 @@ public class MainActivity extends AppCompatActivity {
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextMessage = findViewById(R.id.edit_text_message);
+
+        mediaSession = new MediaSessionCompat(this, "tag");
     }
 
     public void sendOnChannel1(View v) {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
-        /*解碼照片*/
-        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.lotti);
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+
+        Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.flower);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_one)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setLargeIcon(largeIcon)//讓你的Notification有背景圖片
-                /*可以使Notification顯示更多的文字內容。*/
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText(getString(R.string.long_dummy_text))//文字內容
-                        .setBigContentTitle("Big Content Title")//文字標題
-                        .setSummaryText("Summary Text"))//文字摘要
+                .setLargeIcon(picture)
+                .setStyle(new NotificationCompat.BigPictureStyle()//創建一個BigPictureStyle物件，並設定要傳送的圖片
+                        .bigPicture(picture)//設定要傳送的圖片
+                        .bigLargeIcon(null))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
                 .build();
@@ -61,20 +70,22 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
+        Bitmap artwork = BitmapFactory.decodeResource(getResources(), R.drawable.lotti);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_two)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setStyle(new NotificationCompat.InboxStyle()//有點像BigTextStyle，但卻又沒有辦法顯示這麼多文字的內容。
-                        .addLine("This is line 1")
-                        .addLine("This is line 2")
-                        .addLine("This is line 3")
-                        .addLine("This is line 4")
-                        .addLine("This is line 5")
-                        .addLine("This is line 6")
-                        .addLine("This is line 7")
-                        .setBigContentTitle("Big Content Title")
-                        .setSummaryText("Summary Text"))
+                .setLargeIcon(artwork)
+                .addAction(R.drawable.ic_dislike, "Dislike", null)
+                .addAction(R.drawable.ic_previous, "Previous", null)
+                .addAction(R.drawable.ic_pause, "Pause", null)
+                .addAction(R.drawable.ic_next, "Next", null)
+                .addAction(R.drawable.ic_like, "Like", null)
+                .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(1,2,3)//在標準通知視窗之下,設定要顯示那些action
+                        .setMediaSession(mediaSession.getSessionToken()))
+                .setSubText("Sub Text")
                 .setPriority(NotificationCompat.PRIORITY_LOW)
                 .build();
 
