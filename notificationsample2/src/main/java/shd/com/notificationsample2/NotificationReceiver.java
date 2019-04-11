fa47fca94859ed6bb7bdec89f1d55490bff510ae
張宮寶 <mars09250148@gmail.com>
2019-04-11 10:01:40 +0800

    增加sample2的說明,接收notification送出的intent     送出一個broadcast給BroadcastReceiver,讓他在背景做事而不會中斷目前程式的動作NotificationReceiver

--- notificationsample2/src/main/java/shd/com/notificationsample2/NotificationReceiver.java
+++ notificationsample2/src/main/java/shd/com/notificationsample2/NotificationReceiver.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class NotificationReceiver extends BroadcastReceiver {
+    /*接收notification送出的intent
+    送出一個broadcast給BroadcastReceiver,讓他在背景做事而不會中斷目前程式的動作*/
     @Override
     public void onReceive(Context context, Intent intent) {
         String message = intent.getStringExtra("toastMessage");
